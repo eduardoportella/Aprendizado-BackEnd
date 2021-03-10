d9c8a5684943b94747b4b14096c1b4bc4072c112
Eduardo Portella <eportella70@gmail.com>
2021-03-10 11:46:53 -0300

    aula 09 e exercicios

--- python/.aulas/009.py
+++ python/.aulas/009.py
@@ -8,12 +8,14 @@ print(frase[::2])
 print(len(frase))
 print(frase.count('o'))
 print(frase.upper().count('E'))
-print(frase[2][1])
+dividido = frase.split()
+print(dividido[2][3])
 
 frase2 = '      Nao'
 print(frase2)
 frase2 = frase2.split()
-print(frase2.lower().find('nao'))
+print(frase2)
+# print(frase2.lower().find('nao'))
 
 print('\n')
 
