8aa5297c74a6ca2c79df0b955e1364f73c47bdf7
Eduardo Portella <eportella70@gmail.com>
2021-06-18 11:29:49 -0300

    modulos

---
+++ node/calculadoraModulo/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./somar");
+var multiFunc = require("./multi");
+var divFunc = require("./div");
+var subFunc = require("./sub");
+
+console.log(subFunc(2,4));
+console.log(somaFunc(2,4));
+console.log(multiFunc(2,4));
+console.log(divFunc(2,4));
