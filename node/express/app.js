e0ac8aaeb2c4e00a7f028e02fa2841068ec0ba1b
Eduardo Portella <eportella70@gmail.com>
2021-06-26 13:49:52 -0300

    parametros

--- node/express/app.js
+++ node/express/app.js
@@ -13,6 +13,10 @@ app.get("/blog", function(req, res){
     res.send("Bem vindo ao meu blog")
 })
 
+app.get("/ola/:nome/:cargo/:cor", function(req, res){
+    res.send("<h1>Oi, " + req.params.nome + "</h1>"+ "<h2> Seu cargo e: " + req.params.cargo + "</h2>" +"<h3> Sua cor favorita e: "+ req.params.cor + "<h3>")
+})
+
 app.listen(9090, function () {
     console.log("Servidor rodando na url http://localhost:9090");
 });
